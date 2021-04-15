e0cbeb8bba58997b736796fbcf74c3c86bc6148c
joshuaboud <joshuaboud@hotmail.com>
2021-04-15 12:41:20 -0300

    multithread file creation

--- src/impl/gendataset.cpp
+++ src/impl/gendataset.cpp
@@ -77,8 +77,7 @@ boost::random::discrete_distribution<> get_geometric_biased_rand_generator(int d
 	return dist;
 }
 
-std::string *gen_file_paths(int depth, int branches, int count, std::string *dir_names){
-	std::string *file_names = new std::string[count];
+void gen_file_paths(std::vector<std::string> &file_names, int depth, int branches, int count, std::string *dir_names){
 	srand(time(NULL));
 	boost::random::discrete_distribution<> biased_depth = get_geometric_biased_rand_generator(depth, branches);
 	for(int i = 0; i < count; ++i){
@@ -90,70 +89,82 @@ std::string *gen_file_paths(int depth, int branches, int count, std::string *dir
 		}
 		file_names[i] += "file" + std::to_string(i) + ".img";
 	}
-	return file_names;
 }
 
-void touch_files(std::string *file_names, int count, int size, int max_wait_ms){
-	if(size){
-		// create and write
-		int buff_sz = std::min(size, 1024 * 1024);
-		unsigned char *buff = new unsigned char[buff_sz];
-		memset(buff, 0, buff_sz*sizeof(unsigned char));
-		for(int i = 0; i < count; ++i){
-			int fd = creat(file_names[i].c_str(), 0777);
-			if(fd == -1){
+inline void create_file(const char *name, const unsigned char *buff, int buff_sz, int file_sz){
+	int fd = creat(name, 0777);
+	if(fd == -1){
+		int error = errno;
+		std::cerr << "Error creating file: " << strerror(error) << std::endl;
+		delete[] buff;
+		exit(EXIT_FAILURE);
+	}
+	if(buff){
+		int bytes_left = file_sz;
+		while(bytes_left > 0){
+			int bytes_written = write(fd, buff, std::min(buff_sz, bytes_left));
+			if(bytes_written == -1){
 				int error = errno;
-				std::cerr << "Error creating file: " << strerror(error) << std::endl;
+				std::cerr << "Error writing to file: " << strerror(error) << std::endl;
 				delete[] buff;
 				exit(EXIT_FAILURE);
 			}
-			int bytes_left = size;
-			while(bytes_left > 0){
-				int bytes_written = write(fd, buff, std::min(buff_sz, bytes_left));
-				if(bytes_written == -1){
-					int error = errno;
-					std::cerr << "Error writing to file: " << strerror(error) << std::endl;
-					delete[] buff;
-					exit(EXIT_FAILURE);
-				}
-				bytes_left -= bytes_written;
-			}
-			int res = close(fd);
-			if(res == -1){
-				int error = errno;
-				std::cerr << "Error closing file: " << strerror(error) << std::endl;
-				exit(EXIT_FAILURE);
-			}
-			if(max_wait_ms)
-				std::this_thread::sleep_for(std::chrono::milliseconds(rand() % (max_wait_ms+1)));
+			bytes_left -= bytes_written;
 		}
-		delete[] buff;
-	}else{
-		// only create
-		for(int i = 0; i < count; ++i){
-			int fd = creat(file_names[i].c_str(), 0777);
-			if(fd == -1){
-				int error = errno;
-				std::cerr << "Error creating file: " << strerror(error) << std::endl;
-				exit(EXIT_FAILURE);
-			}
-			int res = close(fd);
-			if(res == -1){
-				int error = errno;
-				std::cerr << "Error closing file: " << strerror(error) << std::endl;
-				exit(EXIT_FAILURE);
-			}
-			if(max_wait_ms)
-				std::this_thread::sleep_for(std::chrono::milliseconds(rand() % (max_wait_ms+1)));
-		}
+	}
+	int res = close(fd);
+	if(res == -1){
+		int error = errno;
+		std::cerr << "Error closing file: " << strerror(error) << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+void touch_files(const std::vector<std::string> &file_names, const unsigned char *buff, int buff_sz, int file_sz, int max_wait_ms){
+	for(const std::string &file_name : file_names){
+		create_file(file_name.c_str(), buff, buff_sz, file_sz);
+		if(max_wait_ms)
+			std::this_thread::sleep_for(std::chrono::milliseconds(rand() % (max_wait_ms+1)));
+	}
+}
+
+void launch_threads(int num_threads, const std::vector<std::string> &file_names, const unsigned char *buff, int buff_sz, int file_sz, int max_wait_ms){
+	std::vector<std::vector<std::string>> partitions(
+		num_threads,
+		std::vector<std::string>()
+	);
+	// round robin distribute files
+	int thread_ind = 0;
+	for(unsigned i = 0; i < file_names.size(); ++i){
+		partitions[thread_ind].push_back(file_names[i]);
+		thread_ind = (thread_ind+1)%num_threads;
+	}
+	std::vector<std::thread> threads;
+	for(const std::vector<std::string> &partition : partitions){
+		threads.emplace_back(touch_files, partition, buff, buff_sz, file_sz, max_wait_ms);
+	}
+	for(std::thread &thread : threads){
+		thread.join();
 	}
 }
 
 void gen_dataset(const Options &opts){
 	std::string *dir_names = gen_dir_names(opts.branches);
 	gen_dirs(opts.depth, opts.branches, dir_names, "");
-	std::string *file_names = gen_file_paths(opts.depth+1, opts.branches, opts.count, dir_names);
-	touch_files(file_names, opts.count, opts.size, opts.max_wait_ms);
-	delete[] file_names;
+	std::vector<std::string> file_names(opts.count, "");
+	gen_file_paths(file_names, opts.depth+1, opts.branches, opts.count, dir_names);
+	unsigned char *buff = nullptr;
+	int buff_sz = 0;
+	if(opts.size){
+		buff_sz = std::min(opts.size, 1024 * 1024);
+		buff = new unsigned char[buff_sz];
+		memset(buff, 0, buff_sz*sizeof(unsigned char));
+	}
+	if(opts.threads > 1)
+		launch_threads(opts.threads, file_names, buff, buff_sz, opts.size, opts.max_wait_ms);
+	else
+		touch_files(file_names, buff, buff_sz, opts.size, opts.max_wait_ms);
+	if(opts.size)
+		delete[] buff;
 	delete[] dir_names;
 }
